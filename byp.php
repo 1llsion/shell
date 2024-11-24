fc90470932bc384584b300ee20975198402f46aa
1llsion <156107986+1llsion@users.noreply.github.com>
2024-11-25 00:31:03 +0700

    Update byp.php

--- byp.php
+++ byp.php
@@ -1,52 +1,21 @@
-<?php
-
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return redirect()->route('loic');
-});
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/chart/complaints/byMonth', [\App\Http\Controllers\DashboardController::class, 'getComplaintsByMonth'])->name('chart.getComplaintsByMonth');
-    Route::get('/chart/complaints/byStatus', [\App\Http\Controllers\DashboardController::class, 'getComplaintsByStatus'])->name('chart.getComplaintsByStatus');
-
-    Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('complaints/years', [\App\Http\Controllers\ComplaintController::class, 'getYears'])->name('complaints.getYears');
-    Route::get('complaints/export', [\App\Http\Controllers\ComplaintController::class, 'exportExcel'])->name('complaints.exportExcel');
-    Route::post('complaints/{id}/action', [\App\Http\Controllers\ComplaintController::class, 'takingAction'])->name('complaints.takingAction');
-    Route::post('complaints/{id}/done', [\App\Http\Controllers\ComplaintController::class, 'complaintDone'])->name('complaints.done');
-    Route::resource('complaints', \App\Http\Controllers\ComplaintController::class);
-
-    Route::resource('rules', \App\Http\Controllers\RuleController::class);
-
-    Route::get('users/{id}', [\App\Http\Controllers\UserController::class, 'show'])->name('users.show')->whereUuid('id');
-    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-
-    Route::get('admins/{id}/change-password', [\App\Http\Controllers\AdminController::class, 'changePassword'])->whereUuid('id')->name('admins.change-password');
-    Route::post('admins/{id}/change-password', [\App\Http\Controllers\AdminController::class, 'updatePassword'])->whereUuid('id')->name('admins.update-password');
-    Route::resource('admins', \App\Http\Controllers\AdminController::class);
-});
-
-require __DIR__ . '/auth.php';
-
-// Route::get('/loic', function () {
-//     return view('loic');
-// });
+---
+"email": 'ashilturquoise@awgarstone.com'
+"ip": '103.233.103.194'
+"notify_account_authn_link": 1
+"notify_account_authn_link_notification_disabled": 1
+"notify_account_login": 0
+"notify_account_login_for_known_netblock": 0
+"notify_account_login_notification_disabled": 1
+"notify_autossl_expiry": 1
+"notify_autossl_expiry_coverage": 1
+"notify_autossl_renewal": 1
+"notify_autossl_renewal_coverage": 1
+"notify_contact_address_change": 1
+"notify_contact_address_change_notification_disabled": 1
+"notify_disk_limit": 1
+"notify_password_change": 1
+"notify_password_change_notification_disabled": 1
+"notify_ssl_expiry": 1
+"origin": 'cpanel'
+"pushbullet_access_token": ''
+"second_email": ''

